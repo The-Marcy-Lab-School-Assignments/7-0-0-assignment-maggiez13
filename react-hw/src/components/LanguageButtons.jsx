c0c625f5394763fc3026ed3e22ee8a29d7defc2d
Mei Zhu <maggiez0613@gmail.com>
2024-08-05 19:51:40 -0400

    hw done

--- react-hw/src/components/LanguageButtons.jsx
+++ react-hw/src/components/LanguageButtons.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 function LanguageButtons({ changeLanguage }) {
   return (
-    <div>
+    <>
       <button onClick={() => changeLanguage('English')}>English</button>
       <button onClick={() => changeLanguage('Chinese')}>Chinese</button>
       <button onClick={() => changeLanguage('Spanish')}>Spanish</button>
       <button onClick={() => changeLanguage('Haitian')}>Haitian</button>
       <button onClick={() => changeLanguage('Portuguese')}>Portuguese</button>
-    </div>
+    </>
   );
 }
 
